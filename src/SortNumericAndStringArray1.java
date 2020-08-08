e3cf21faac34a742822107e7440e89c056fa52b1
naynaborda <naynaborda@gmail.com>
2020-08-08 12:45:30 +0100

    added comment

--- src/SortNumericAndStringArray1.java
+++ src/SortNumericAndStringArray1.java
@@ -2,6 +2,8 @@ import java.util.Arrays;
 
 public class SortNumericAndStringArray1 {
     //1.WAP in Java  to sort a numeric array and string array.
+    // Testing the git hub
+
     public static void main(String[] args) {
         int array[] = {19, 67, 56, 89, 70, 79, 92};
         String str[] = {"Ram", "Krishna", "Sita", "Laxman", "Bharat", "Dashrath"};
